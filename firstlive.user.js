12cf646eba7b33c319dfb1e0b6f2d832a81ef3d6
Baisela <33560723+Baisela@users.noreply.github.com>
2018-01-21 06:26:28 +0100

    Update firstlive.user.js

--- firstlive.user.js
+++ firstlive.user.js
@@ -2,8 +2,8 @@
 // @name First Live
 // @description Refresh les topics de la première page et le compteur de connecté toutes les X secondes si le curseur de la souris n'est pas sur la liste des sujets
 // @author baisela
-// @match www.jeuxvideo.com/forums/0-51-0-1-0-1-0-blabla-18-25-ans.htm
-// @match m.jeuxvideo.com/forums/0-51-0-1-0-1-0-blabla-18-25-ans.htm
+// @match http://www.jeuxvideo.com/forums/0-51-0-1-0-1-0-blabla-18-25-ans.htm
+// @match http://m.jeuxvideo.com/forums/0-51-0-1-0-1-0-blabla-18-25-ans.htm
 // @run-at document-end
 // @require https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @version 1.0
@@ -26,6 +26,7 @@ setInterval(function(){
             $.get(location.href).then(function(page) {
                 $(".nb-connect-fofo").text($(page).find(".nb-connect-fofo").text());
             });
+            Console.log("test");
         }
     }
     else{
