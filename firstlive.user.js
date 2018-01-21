9a3550fa8abeef3f6d877d9284d7bb07fb83229b
Baisela <33560723+Baisela@users.noreply.github.com>
2018-01-21 06:19:28 +0100

    Update firstlive.user.js

--- firstlive.user.js
+++ firstlive.user.js
@@ -6,23 +6,29 @@
 // @match m.jeuxvideo.com/forums/0-51-0-1-0-1-0-blabla-18-25-ans.htm
 // @run-at document-end
 // @require https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// @version 1.1
+// @version 1.0
 // ==/UserScript==
 
-var timer = 14; //secondes
+var timer = 15; //secondes
 var timeOut = 0;
 
-$(".topic-list.topic-list-admin").mouseenter(function(){
-    timeOut = 1;
-}).mouseleave(function(){
-    timeOut = 0;
-});
+if(location.href == "http://www.jeuxvideo.com/forums/0-51-0-1-0-1-0-blabla-18-25-ans.htm"){
+    $(".topic-list.topic-list-admin").mouseenter(function(){
+        timeOut = 1;
+    }).mouseleave(function(){
+        timeOut = 0;
+    });
+}
 
 setInterval(function(){
-    if(!timeOut){
-        $(".topic-list.topic-list-admin").load(location.href + " .topic-list.topic-list-admin > *");
-        $.get(location.href).then(function(page) {
-            $(".nb-connect-fofo").text($(page).find(".nb-connect-fofo").text());
-        });
+    if(location.href == "http://www.jeuxvideo.com/forums/0-51-0-1-0-1-0-blabla-18-25-ans.htm"){
+        if(!timeOut){
+            $.get(location.href).then(function(page) {
+                $(".nb-connect-fofo").text($(page).find(".nb-connect-fofo").text());
+            });
+        }
+    }
+    else{
+        $(".liste-topics").load(location.href + " .liste-topics > *");
     }
 }, timer*1000);
